47cb83bb1a8193d93c0441d87a02e1c11dfcd195
Ramprasath Manoharan <ramprasath.manoharan@athenatec.com>
2026-02-23 14:02:41 +0530

    chat bot added and eyelit page updated

---
+++ app/api/chat/route.ts
@@ -0,0 +1,30 @@
+export async function POST(req: Request) {
+  try {
+    const { message } = await req.json();
+
+    const response = await fetch("http://127.0.0.1:11434/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "llama3",
+        prompt: message,
+        stream: false,
+      }),
+    });
+
+    const data = await response.json();
+
+    return new Response(
+      JSON.stringify({ reply: data.response }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error("OLLAMA ERROR:", error);
+    return new Response(
+      JSON.stringify({ error: "Something went wrong" }),
+      { status: 500 }
+    );
+  }
+}
---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+export default function HeroSection({
+  title,
+  description,
+  image,
+  buttonText,
+  buttonLink,
+  align = "center",
+}) {
+  const router = useRouter();
+
+  return (
+    <section className="relative w-full min-h-[70vh] flex items-center justify-center overflow-hidden text-white">
+      <Image src={image} alt={title} fill priority className="object-cover" />
+
+      <div
+        className={`relative z-20 max-w-5xl px-6 flex flex-col ${
+          align === "left"
+            ? "items-start text-left"
+            : "items-center text-center"
+        }`}
+      >
+        <h1 className="text-4xl md:text-6xl font-bold mb-6">{title}</h1>
+
+        {description && (
+          <p className="text-lg md:text-xl mb-8 max-w-3xl">{description}</p>
+        )}
+
+        {buttonText && buttonLink && (
+          <button
+            onClick={() => router.push(buttonLink)}
+            className="    px-8 py-3
+    bg-[#1c4584]
+    text-white
+    font-semibold
+    rounded-lg
+    transition-all duration-300 ease-in-out
+    hover:bg-[#1f54a5]
+    hover:-translate-y-1
+    hover:shadow-xl
+    active:translate-y-0"
+          >
+            {buttonText}
+          </button>
+        )}
+      </div>
+    </section>
+  );
+}
